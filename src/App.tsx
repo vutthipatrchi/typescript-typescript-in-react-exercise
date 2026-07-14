4ac33418d7d4866244b03fbb5424ee143c221d49
Vutthipate <yourname@example.com>
2026-07-14 14:27:08 +0700

    implement routing for Home and UsersList pages; define types for HomeProps and User

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import UsersList from "./pages/UserList";
 
 function App() {
   return (
     <Router>
       <Routes>
-        // TODO3: เพิ่ม Route ไป Home ตรงนี้
-        <Route path="/" element={} />
-        // TODO4: เพิ่ม Route ไป UsersList ตรงนี้
-        <Route path="/users" element={} />
+        <Route path="/" element={<Home name="Alice" />} />
+        <Route path="/users" element={<UsersList />} />
       </Routes>
     </Router>
   );
